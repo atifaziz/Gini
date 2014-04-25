fd688ab7eba38cc8aa977cf42cdf95c64e741be7
Atif Aziz <code@raboof.com>
2014-04-25 23:33:54 +0200

    Pairwise was public when should have been private

--- Ini.cs
+++ Ini.cs
@@ -172,7 +172,7 @@ namespace Gini
         // See the License for the specific language governing permissions and
         // limitations under the License.
 
-        public static IEnumerable<TResult> Pairwise<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TSource, TResult> resultSelector)
+        static IEnumerable<TResult> Pairwise<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TSource, TResult> resultSelector)
         {
             if (source == null) throw new ArgumentNullException("source");
             if (resultSelector == null) throw new ArgumentNullException("resultSelector");
