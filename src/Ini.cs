5ec9c4513f8e8a55ec5c99fbd0ae481c1539e556
Atif Aziz <code@raboof.com>
2018-10-10 13:41:36 +0200

    Add EditorConfig
    See https://editorconfig.org/

--- src/Ini.cs
+++ src/Ini.cs
@@ -1,17 +1,17 @@
 #region The MIT License (MIT)
 //
 // Copyright (c) 2013 Atif Aziz. All rights reserved.
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
 // the Software without restriction, including without limitation the rights to
 // use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 // the Software, and to permit persons to whom the Software is furnished to do so,
 // subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 // FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -38,7 +38,7 @@ namespace Gini
     #if GINI_PUBLIC
     public partial class Ini { }
     #endif
-    
+
     static partial class Ini
     {
         static class Parser
@@ -98,7 +98,7 @@ namespace Gini
                         Setting = KeyValuePair.Create(k, v)
                     })
                 }
-                from p in Enumerable.Repeat(new { Section = (string) null, 
+                from p in Enumerable.Repeat(new { Section = (string) null,
                                                   Setting = KeyValuePair.Create(string.Empty, string.Empty) }, 1)
                                     .Concat(ms)
                                     .GroupAdjacent(s => s.Section == null || s.Section.Length > 0)
@@ -159,13 +159,13 @@ namespace Gini
 
         // MoreLINQ - Extensions to LINQ to Objects
         // Copyright (c) 2008 Jonathan Skeet. All rights reserved.
-        // 
+        //
         // Licensed under the Apache License, Version 2.0 (the "License");
         // you may not use this file except in compliance with the License.
         // You may obtain a copy of the License at
-        // 
+        //
         //     http://www.apache.org/licenses/LICENSE-2.0
-        // 
+        //
         // Unless required by applicable law or agreed to in writing, software
         // distributed under the License is distributed on an "AS IS" BASIS,
         // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -302,7 +302,7 @@ namespace Gini
         }
 
         #endregion
-    }    
+    }
 }
 
 #if GINI_DYNAMIC
